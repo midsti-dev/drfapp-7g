654d447b483d43cc07f36cfda4ce715c78c81cdf
Izbergenov Dauletkhan <izbergenovdauletkhan@MacBook-Pro-Izbergenov.local>
2025-12-08 01:43:26 +0500

    k8s.dir-1.1-added

--- src/settings.py
+++ src/settings.py
@@ -151,9 +151,9 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.BasicAuthentication",
     ),
-    "DEFAULT_PERMISSION_CLASSES": {
+    "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.AllowAny",
-    },
+    ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema"
 }
 
